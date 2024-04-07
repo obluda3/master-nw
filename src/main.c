0ff1fa39b0fee1f7478db3eb6519a55224ca56a4
obluda3 <yoshivert992@gmail.com>
2024-04-07 17:56:31 +0200

    Default memory

--- src/main.c
+++ src/main.c
@@ -12,27 +12,29 @@ const uint32_t eadk_api_level  __attribute__((section(".rodata.eadk_api_level"))
 
 int main(int argc, char * argv[]) {
   Emu emu;
-  char rom[0x8000];
+  char* rom;
   #ifndef TARGET_LINUX
   rom = eadk_external_data;
   eadk_display_push_rect_uniform((eadk_rect_t){0, 0, 320, 240}, eadk_color_white);
   #else
-  /*
+  
   InitWindow(512, 384, "master-nw");
   SetTargetFPS(60);
   FILE *file = fopen(argv[1], "rb");
   fseek(file, 0, SEEK_END);
 	unsigned long len = ftell(file);
 	fseek(file, 0, SEEK_SET);
+  printf("len=%x\n", len);
   rom=(char *)malloc(len+1);
   fread(rom, len, 1, file);
 	fclose(file);
-  */
+  
 
   #endif
 
   init_cpu(&emu.cpu);
   init_mem(&emu.mem, rom);
   vdp_init(&emu.vdp);
-  unit_test(&emu, "tests/test_data/00.json.bin");
+  emu_loop(&emu);
+  return 1;
 }
